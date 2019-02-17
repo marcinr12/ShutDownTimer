d277fb99f18a7eca543bf4a172c605c3c304bbd8
marcinr12 <marcinr12@gmial.com>
2019-02-17 16:40:21 +0100

    Addind application

---
+++ ShutdownApp/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace ShutdownApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.button1.Location = new System.Drawing.Point(177, 66);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(143, 50);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Shut down";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.button2.Location = new System.Drawing.Point(12, 66);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(143, 50);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Cancel";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Checked = false;
+            this.dateTimePicker1.CustomFormat = "HH:mm:ss dd-MM-yyyy";
+            this.dateTimePicker1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePicker1.Location = new System.Drawing.Point(12, 12);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.ShowUpDown = true;
+            this.dateTimePicker1.Size = new System.Drawing.Size(308, 38);
+            this.dateTimePicker1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F);
+            this.label1.Location = new System.Drawing.Point(12, 136);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 58);
+            this.label1.TabIndex = 3;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(19, 210);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 17);
+            this.label2.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(332, 236);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Simple SD";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ ShutdownApp/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ShutdownApp
+{
+    public partial class Form1 : Form
+    {
+        private System.Windows.Forms.Timer timer = null;
+        ulong counter = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string strCmdText = "shutdown -s -t ";       
+
+            DateTime sdTime = dateTimePicker1.Value;
+            DateTime currentTime = DateTime.Now;
+
+            int compare = DateTime.Compare(sdTime, currentTime);
+            if (compare < 0)
+            {
+                //MessageBox.Show("Incorrect time");
+                label2.Text = "Incorrect time!";
+                return;
+            }
+
+            
+            ulong dSeconds =  (ulong)(sdTime - currentTime).TotalSeconds;
+            label1.Text = counter.ToString();
+            counter = (ulong)dSeconds;
+            timer = new System.Windows.Forms.Timer();
+            timer.Tick += new EventHandler(timer1_Tick);
+            timer.Interval = 1000; // 1 second
+            timer.Start();
+            //label1.Text = dSeconds.ToString();
+
+            // /K -> CMD opened
+            // /C -> CMD closed
+            System.Diagnostics.Process.Start("CMD.exe", "/C" + strCmdText + dSeconds.ToString());
+        }
+
+
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string strCmdText = "shutdown -a";
+            System.Diagnostics.Process.Start("CMD.exe", "/C" + strCmdText);
+            if(timer == null || !timer.Enabled)
+            {
+                label2.Text = "Operation is not executing!";
+                return;
+            }
+            timer.Stop();
+            label1.Text = "";
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            --counter;
+            if (counter < 0)
+                timer.Stop();
+            else if (counter > 0)
+                label1.Text = timeSpanConvertion(counter);//counter.ToString();
+            
+        }
+
+        private string timeSpanConvertion(ulong secs)
+        {
+            TimeSpan t = TimeSpan.FromSeconds(secs);
+            string answer = string.Format("{0:D2}h:{1:D2}m:{2:D2}s", t.Hours, t.Minutes, t.Seconds);
+            return answer;
+        }
+
+    }
+}
