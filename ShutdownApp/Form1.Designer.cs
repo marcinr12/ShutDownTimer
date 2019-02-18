8b5610c8b5210bbf35b641b926bb5b5369d202b0
marcinr12 <marcinr12@gmial.com>
2019-02-18 20:43:02 +0100

    Adding shut down after time feature.

--- ShutdownApp/Form1.Designer.cs
+++ ShutdownApp/Form1.Designer.cs
@@ -36,12 +36,23 @@
             this.label1 = new System.Windows.Forms.Label();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.label2 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.button1.Location = new System.Drawing.Point(177, 66);
+            this.button1.Location = new System.Drawing.Point(177, 107);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(143, 50);
             this.button1.TabIndex = 0;
@@ -52,7 +63,7 @@
             // button2
             // 
             this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.button2.Location = new System.Drawing.Point(12, 66);
+            this.button2.Location = new System.Drawing.Point(12, 107);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(143, 50);
             this.button2.TabIndex = 1;
@@ -66,20 +77,22 @@
             this.dateTimePicker1.CustomFormat = "HH:mm:ss dd-MM-yyyy";
             this.dateTimePicker1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
-            this.dateTimePicker1.Location = new System.Drawing.Point(12, 12);
+            this.dateTimePicker1.Location = new System.Drawing.Point(36, 12);
             this.dateTimePicker1.Name = "dateTimePicker1";
             this.dateTimePicker1.ShowUpDown = true;
-            this.dateTimePicker1.Size = new System.Drawing.Size(308, 38);
+            this.dateTimePicker1.Size = new System.Drawing.Size(284, 38);
             this.dateTimePicker1.TabIndex = 2;
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F);
-            this.label1.Location = new System.Drawing.Point(12, 136);
+            this.label1.Location = new System.Drawing.Point(2, 160);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(0, 58);
+            this.label1.Size = new System.Drawing.Size(67, 58);
             this.label1.TabIndex = 3;
+            this.label1.Text = "...";
             // 
             // timer1
             // 
@@ -88,16 +101,118 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(19, 210);
+            this.label2.Location = new System.Drawing.Point(9, 218);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(0, 17);
+            this.label2.Size = new System.Drawing.Size(20, 17);
             this.label2.TabIndex = 4;
+            this.label2.Text = "...";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.numericUpDown1.Location = new System.Drawing.Point(36, 63);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            24,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(45, 38);
+            this.numericUpDown1.TabIndex = 5;
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.numericUpDown2.Location = new System.Drawing.Point(127, 63);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            24,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(45, 38);
+            this.numericUpDown2.TabIndex = 6;
+            this.numericUpDown2.ValueChanged += new System.EventHandler(this.numericUpDown2_ValueChanged);
+            // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.numericUpDown3.Location = new System.Drawing.Point(220, 63);
+            this.numericUpDown3.Maximum = new decimal(new int[] {
+            24,
+            0,
+            0,
+            0});
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(45, 38);
+            this.numericUpDown3.TabIndex = 7;
+            this.numericUpDown3.ValueChanged += new System.EventHandler(this.numericUpDown3_ValueChanged);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(12, 23);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(18, 17);
+            this.checkBox1.TabIndex = 8;
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(12, 73);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(18, 17);
+            this.checkBox2.TabIndex = 9;
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.label3.Location = new System.Drawing.Point(87, 70);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(34, 31);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "H";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.label4.Location = new System.Drawing.Point(178, 70);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(36, 31);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "M";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.label5.Location = new System.Drawing.Point(271, 70);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(32, 31);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "S";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(332, 236);
+            this.ClientSize = new System.Drawing.Size(332, 254);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.numericUpDown3);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.dateTimePicker1);
@@ -108,6 +223,9 @@
             this.MaximizeBox = false;
             this.Name = "Form1";
             this.Text = "Simple SD";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -121,6 +239,14 @@
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.NumericUpDown numericUpDown3;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
     }
 }
 
--- ShutdownApp/Form1.cs
+++ ShutdownApp/Form1.cs
@@ -22,32 +22,13 @@ namespace ShutdownApp
 
         private void button1_Click(object sender, EventArgs e)
         {
-            string strCmdText = "shutdown -s -t ";       
+            if (checkBox1.Checked == true)
+                shutDownAt();
+            if (checkBox2.Checked == true)
+                shutDownAfter();
+            if (checkBox1.Checked == false && checkBox2.Checked == false)
+                label2.Text = "Please select shut down option!";
 
-            DateTime sdTime = dateTimePicker1.Value;
-            DateTime currentTime = DateTime.Now;
-
-            int compare = DateTime.Compare(sdTime, currentTime);
-            if (compare < 0)
-            {
-                //MessageBox.Show("Incorrect time");
-                label2.Text = "Incorrect time!";
-                return;
-            }
-
-            
-            ulong dSeconds =  (ulong)(sdTime - currentTime).TotalSeconds;
-            label1.Text = counter.ToString();
-            counter = (ulong)dSeconds;
-            timer = new System.Windows.Forms.Timer();
-            timer.Tick += new EventHandler(timer1_Tick);
-            timer.Interval = 1000; // 1 second
-            timer.Start();
-            //label1.Text = dSeconds.ToString();
-
-            // /K -> CMD opened
-            // /C -> CMD closed
-            System.Diagnostics.Process.Start("CMD.exe", "/C" + strCmdText + dSeconds.ToString());
         }
 
 
@@ -72,7 +53,7 @@ namespace ShutdownApp
             if (counter < 0)
                 timer.Stop();
             else if (counter > 0)
-                label1.Text = timeSpanConvertion(counter);//counter.ToString();
+                label1.Text = timeSpanConvertion(counter);
             
         }
 
@@ -83,5 +64,99 @@ namespace ShutdownApp
             return answer;
         }
 
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked == true)
+            {
+                checkBox2.Checked = false;
+                label2.Text = "";
+            }
+                
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox2.Checked == true)
+            {
+                checkBox1.Checked = false;
+                label2.Text = "";
+            }
+                
+        }
+
+        public void shutDownAt()
+        {
+            string strCmdText = "shutdown -s -t ";
+
+            DateTime sdTime = dateTimePicker1.Value;
+            DateTime currentTime = DateTime.Now;
+
+            int compare = DateTime.Compare(sdTime, currentTime);
+            if (compare < 0)
+            {
+                //MessageBox.Show("Incorrect time");
+                label2.Text = "Incorrect time!";
+                return;
+            }
+
+
+            ulong dSeconds = (ulong)(sdTime - currentTime).TotalSeconds;
+            label1.Text = counter.ToString();
+            counter = (ulong)dSeconds;
+            timer = new System.Windows.Forms.Timer();
+            timer.Tick += new EventHandler(timer1_Tick);
+            timer.Interval = 1000;      // 1 second
+            timer.Start();
+
+            // /K -> CMD opened
+            // /C -> CMD closed
+            System.Diagnostics.Process.Start("CMD.exe", "/C" + strCmdText + dSeconds.ToString());
+        }
+
+        public void shutDownAfter()
+        {
+            uint dHours = (uint)numericUpDown1.Value;
+            uint dMinutes = (uint)numericUpDown2.Value;
+            ulong dSeconds = (ulong)numericUpDown3.Value;
+
+            dSeconds = dMinutes * 60 + dHours * 60 * 60;
+
+            if (dSeconds == 0)
+                return;
+
+            counter = (ulong)dSeconds;
+            timer = new System.Windows.Forms.Timer();
+            timer.Tick += new EventHandler(timer1_Tick);
+            timer.Interval = 1000;      // 1 second
+            timer.Start();
+
+            string strCmdText = "shutdown -s -t ";
+
+            System.Diagnostics.Process.Start("CMD.exe", "/C" + strCmdText + dSeconds);
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            checkBox1.Checked = true;
+            checkBox2.Checked = false;
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            checkBox1.Checked = false;
+            checkBox2.Checked = true;
+        }
+
+        private void numericUpDown2_ValueChanged(object sender, EventArgs e)
+        {
+            checkBox1.Checked = false;
+            checkBox2.Checked = true;
+        }
+
+        private void numericUpDown3_ValueChanged(object sender, EventArgs e)
+        {
+            checkBox1.Checked = false;
+            checkBox2.Checked = true;
+        }
     }
 }
